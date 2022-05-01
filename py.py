e81b5606c96f6c5a0aba45722b0f26b4c4014491
Maks <mpetrynko@gmail.com>
2022-05-01 09:00:22 -1000

    2 commit

--- py.py
+++ py.py
@@ -28,20 +28,23 @@ def save_data_to_db(data):
 
         connection.commit()
 
-
-def save_page(url, file_name):
-    driver = webdriver.Chrome('../chromedriver/chromedriver')
+def get_html(url):
+    driver = webdriver.Chrome('./chromedriver/chromedriver')
 
     try:
         driver.get(url=url)
-        sleep(2)
-        with open(file_name, 'wt') as file:
-            file.write(driver.page_source)
+        sleep(3)
+        result = driver.page_source
     except Exception as e:
         print(e)
     finally:
         driver.close()
         driver.quit()
+    return result
+
+def save_page(html, file_name):
+    with open(file_name, 'wt') as file:
+        file.write(driver.page_source)
 
 
 def get_html_from_file(file_name):
@@ -56,28 +59,64 @@ def scrap(html):
     for section in sections:
         data = {}
 
-        data['upwork_id'] = section.['id']
+        data['upwork_id'] = section['id']
         data['title'] = section.find('div', class_='row my-10').find('h4').text.replace('\'', '')
         data['description'] = section.find('div', class_='mb-10').find('div', class_='mt-10').text.replace('\'', '')
 
+        print('{}: {}'.format(data['upwork_id'], data['title']))
         result.append(data)
     return result
 
+bad_page_count = 0
+def check_page(html):
+    global bad_page_count
+    soup = BeautifulSoup(html, 'lxml')
+    if soup.find('title').text == 'Freelance Jobs - Upwork':
+        bad_page_count += 1
+        print(f'Bad page {bad_page_count}')
+        return True
+    return False
+
+
 
 def main():
-    url = 'https://www.upwork.com/nx/jobs/search/?sort=recency&per_page=50&page={}'
+    dont_scraped_urls = []
+    while True:
+        i = 0
+        url = 'https://www.upwork.com/nx/jobs/search/?sort=recency&per_page=50&page={}'
+        directory_path = './pages'
 
-    directory_path = './pages'
-    for page in range(1, 501):
-        save_page(url=url.format(page), file_name=f'{directory_path}/page_{page}.html')
-        print(f'Page [{page}] --')
-        sleep(4)
-        html = get_html_from_file(f'{directory_path}/page_{page}.html')
-        data = scrap(html)
-        with open('shos.txt', 'wt') as file:
-            file.write(str(data))
-        save_data_to_db(data)
-    sleep(3000)
+        while dont_scraped_urls:
+            i += 1
+            url = dont_scraped_urls.pop(0)
+            html = get_html(url=url)
+            print(f'page {i}')
+            # save_page(url=url, file_name=f'{directory_path}/page.html')
+            # html = get_html_from_file(f'{directory_path}/page.html')
+
+            if not check_page(html):
+                dont_scraped_urls.append(current_url)
+                continue
+            data = scrap(html)
+            save_data_to_db(data)
+            sleep(4)
+
+        for page in range(1, 501):
+            i+=1
+            print(f'page {i}')
+            current_url = url.format(page)
+            html = get_html(url=current_url)
+            # save_page(html=html, file_name=f'{directory_path}/page_{page}.html')
+            # html = get_html_from_file(f'{directory_path}/page.html')
+
+            if not check_page(html):
+                dont_scraped_urls.append(current_url)
+                continue
+
+            data = scrap(html)
+            save_data_to_db(data)
+            sleep(4)
+
 
 
 
